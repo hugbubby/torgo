6002d7bc67cb752906e6f9c7ee7067b8c32b466a
dean <dean@dean.com>
2019-12-20 15:19:08 -0600

    Added some util code

---
+++ util.go
@@ -0,0 +1,16 @@
+package torgo
+
+import (
+	"regexp"
+	"strings"
+)
+
+func isValidHiddenServiceId(service string) bool {
+	service = strings.TrimSuffix(service, ".onion")
+	if match, _ := regexp.Match("^[a-z2-7]{16}$", []byte(service)); match {
+		return true
+	} else if match, _ := regexp.Match("^[a-z2-7]{56}$", []byte(service)); match {
+		return true
+	}
+	return false
+}
